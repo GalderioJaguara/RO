f0877e8924c2fc7169b37cefdaf4f3cf0e598848
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-04-24 13:52:13 -0300

    Criação de middlewar de Error handling e conexão com banco de dados

--- RO.DevTest.Application/middlewares/ErrorHandlingMiddleware.cs
+++ RO.DevTest.Application/middlewares/ErrorHandlingMiddleware.cs
@@ -1,9 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RO.DevTest.Domain.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace RO.DevTest.Application.Middlewares {
+    public class ErrorHandlingMiddleware {
 
-namespace RO.DevTest.Application.Middlewares;
+        private readonly RequestDelegate next;
 
-public abstract class ErrorHandlingMiddleware {
-    private readonly RequestDelegate next;
+        public ErrorHandlingMiddleware(RequestDelegate next) {
+            this.next = next;
+        }
 
+        public async Task Invoke(HttpContext context) {
+            try {
+                await next(context);
+            } catch (Exception exception) {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception) {
+            var code = HttpStatusCode.InternalServerError;
+
+            if (exception is ApiException apiException) {
+                context.Response.StatusCode = (int)apiException.StatusCode;
+                context.Response.ContentType = "application/json";
+
+                var result = JsonConvert.SerializeObject(new {
+                    errors = apiException.Errors
+                });
+                return context.Response.WriteAsync(result);
+            }
+
+            var genericResult = JsonConvert.SerializeObject(new {
+                errors = new[] { "An unexpected error occurred." }
+            });
+            context.Response.StatusCode = (int)code;
+            context.Response.ContentType = "application/json";
+            return context.Response.WriteAsync(genericResult);
+        }
+    }
 }
--- RO.DevTest.WebApi/Program.cs
+++ RO.DevTest.WebApi/Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using RO.DevTest.Application;
+using RO.DevTest.Application.Middlewares;
+using RO.DevTest.Domain.Exception;
 using RO.DevTest.Infrastructure.IoC;
+using RO.DevTest.Persistence;
 using RO.DevTest.Persistence.IoC;
+using System.Net;
+
 
 namespace RO.DevTest.WebApi;
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddDbContext<DefaultContext>(options => options.UseNpgsql(connectionString));
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -23,16 +34,34 @@ public class Program {
                 typeof(Program).Assembly
             );
         });
-
-        
+        builder.Services.AddCors(options => {
+            options.AddPolicy("AllowAll", builder => {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+        });
 
         var app = builder.Build();
 
+        using (var scope = app.Services.CreateScope()) {
+            var services = scope.ServiceProvider;
+            try {
+                var context = services.GetRequiredService<DefaultContext>();
+                context.Database.Migrate();
+               
+            }
+            catch (Exception ex) {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "Ocorreu um erro durante a migração do banco de dados.");
+            }
+        }
+
         // Configure the HTTP request pipeline.
-        if(app.Environment.IsDevelopment()) {
+        if (app.Environment.IsDevelopment()) {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.UseMiddleware(typeof(ErrorHandlingMiddleware));
 
         app.UseHttpsRedirection();
 
